8472d464e467ec0eccaa722dc1dd46ec1c09e67c
Stephan Stanisic <stephanstanisic@gmail.com>
2019-11-07 13:36:51 +0000

    Fixed paths for static assets. (When visiting pages that are not next to the website root.)

--- summernote-editor.php
+++ summernote-editor.php
@@ -43,10 +43,10 @@ function initialSummerNoteVariables($contents) {
 function loadSummerNoteJS($args) {
     global $Wcms;
     if ($Wcms->loggedIn) {
-        $script = <<<'EOT'
+        $script = <<<EOT
         <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.8/dist/summernote.min.js" integrity="sha384-rv18XCwOLyh2rO/6Vz9eKWaSP+ljKfFed/npYlSg476id+996jfNDF+5iC3W5KKJ" crossorigin="anonymous"></script>
-        <script src="plugins/summernote-editor/js/admin.js" type="text/javascript"></script>
-        <script src="plugins/summernote-editor/js/files.js" type="text/javascript"></script>
+        <script src="{$Wcms->url('plugins/summernote-editor/js/admin.js')}" type="text/javascript"></script>
+        <script src="{$Wcms->url('plugins/summernote-editor/js/files.js')}" type="text/javascript"></script>
 EOT;
         $args[0] .= $script;
     }
@@ -56,9 +56,9 @@ EOT;
 function loadSummerNoteCSS($args) {
     global $Wcms;
     if ($Wcms->loggedIn) {
-        $script = <<<'EOT'
+        $script = <<<EOT
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/summernote@0.8.11/dist/summernote.css" integrity="sha384-hHEPAd3Dkb316VuNPtIZ6LUrzxvD4PQOTW478Ww6c/aUJKXNDV9pEx5/jZgISR1G" crossorigin="anonymous">
-        <link rel="stylesheet" href="plugins/summernote-editor/css/admin.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="{$Wcms->url('plugins/summernote-editor/css/admin.css')}" type="text/css" media="screen">
 EOT;
         $args[0] .= $script;
     }
