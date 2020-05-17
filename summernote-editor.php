5237ca762ec38d94e831c87933cc54d331c33b39
Robert Isoski <r@robiso.com>
2020-05-17 18:23:07 +0200

    Add files via upload

--- summernote-editor.php
+++ summernote-editor.php
@@ -47,6 +47,8 @@ function loadSummerNoteJS($args) {
         <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.8/dist/summernote.min.js" integrity="sha384-rv18XCwOLyh2rO/6Vz9eKWaSP+ljKfFed/npYlSg476id+996jfNDF+5iC3W5KKJ" crossorigin="anonymous"></script>
         <script src="{$Wcms->url('plugins/summernote-editor/js/admin.js')}" type="text/javascript"></script>
         <script src="{$Wcms->url('plugins/summernote-editor/js/files.js')}" type="text/javascript"></script>
+        <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote-bs4.min.js"></script>
+
 EOT;
         $args[0] .= $script;
     }
@@ -59,6 +61,7 @@ function loadSummerNoteCSS($args) {
         $script = <<<EOT
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/summernote@0.8.11/dist/summernote.css" integrity="sha384-hHEPAd3Dkb316VuNPtIZ6LUrzxvD4PQOTW478Ww6c/aUJKXNDV9pEx5/jZgISR1G" crossorigin="anonymous">
         <link rel="stylesheet" href="{$Wcms->url('plugins/summernote-editor/css/admin.css')}" type="text/css" media="screen">
+        <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote-bs4.min.css" rel="stylesheet">
 EOT;
         $args[0] .= $script;
     }
