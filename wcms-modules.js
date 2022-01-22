52b265ddfc332f31fda10f02b7fa66a879bb381f
Robert Isoski <r@robiso.com>
2022-01-22 12:48:17 +0100

    Add files via upload

---
+++ wcms-modules.js
@@ -0,0 +1,13 @@
+{
+    "version": 1,
+    "plugins": {
+        "summernote-editor": {
+            "name": "Summernote Editor",
+            "repo": "https://github.com/robiso/summernote-editor/tree/master",
+            "zip": "https://github.com/robiso/summernote-editor/archive/master.zip",
+            "summary": "Adds an WYSIWYG editor to all content editable fields.",
+            "version": "3.2.1",
+            "image": "https://raw.githubusercontent.com/robiso/summernote-editor/master/preview.jpg"
+        },
+    }
+}
