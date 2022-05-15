c32862227f25ee156351d514d3b8cfcfa2a725d5
Robert Isoski <r@robiso.com>
2022-05-15 20:17:07 +0200

    Update admin.js
    Adding "image processing", not base64 enconding images anymore, directly uploaded to data/files. Thanks to @slavenstancic

--- js/admin.js
+++ js/admin.js
@@ -19,33 +19,54 @@ $(function() {
         ],
         placeholder: 'Click here to enter content.',
         callbacks: {
+            onImageUpload: async function (files) {
+                for (const file of files) {
+                    const formData = new FormData();
+
+                    formData.append("token", token);
+                    formData.append("uploadFile", file);
+                    $("#save").show();
+                    await fetch('', {method: "POST", body: formData});
+
+                    const imgNode = document.createElement('img');
+                    imgNode.src = `${rootURL}data/files/${file.name}`;
+                    $('#save').fadeOut();
+                    $('.editable').summernote('insertNode', imgNode);
+                    saveData($(this));
+                }
+            },
             onChange: function(contents, $editable) {
                 editElements[$(this).attr('id')] = contents;
             },
             onBlur: function() {
-                if (editElements[$(this).attr('id')]!=undefined) {
-                    // Confirmation popup for saving changes (set in the database)
-                    if (typeof saveChangesPopup !== 'undefined' && saveChangesPopup && !confirm('Save new changes?')) {
-                        alert("Changed are not saved, you can continue to edit or refresh the page.");
-                        return
-                    }
-
-                    var id = $(this).attr('id');
-                    var content = editElements[$(this).attr('id')];
-                    var target = ($(this).attr('data-target')!=undefined) ? $(this).attr('data-target'):'pages';
-                    editElements[$(this).attr('id')] = undefined;
-                    $.post("",{
-                        fieldname: id,
-                        content: content,
-                        target: target,
-                        token: token,
-                    })
-                    .done(function() {
-                        $("#save").show();
-                        $('#save').delay(100).fadeOut(); 
-                    });
-                }
+                saveData($(this));
             }
         },
     });
+
+    function saveData(editor) {
+        console.log(editElements[editor.attr('id')])
+        if (editElements[editor.attr('id')]!=undefined) {
+            // Confirmation popup for saving changes (set in the database)
+            if (typeof saveChangesPopup !== 'undefined' && saveChangesPopup && !confirm('Save new changes?')) {
+                alert("Changed are not saved, you can continue to edit or refresh the page.");
+                return
+            }
+
+            var id = editor.attr('id');
+            var content = editElements[editor.attr('id')];
+            var target = (editor.attr('data-target')!=undefined) ? editor.attr('data-target'):'pages';
+            editElements[editor.attr('id')] = undefined;
+            $.post("",{
+                fieldname: id,
+                content: content,
+                target: target,
+                token: token,
+            })
+              .done(function() {
+                  $("#save").show();
+                  $('#save').delay(100).fadeOut();
+              });
+        }
+    }
 });
